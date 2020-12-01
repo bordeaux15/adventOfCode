13b9172820a4e63d13ccd381a3334ca4267d743f
Matt Borto <Matthew.Borto@analog.com>
2020-12-01 12:08:31 -0500

    Optimize solution with modules
    Include the sample text with the directory for easier debug.
    Added an improved version of the code that used the Algorithm module

---
+++ 2020/01/day1_solution_improved.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env/perl
+
+use strict;
+use warnings;
+
+use lib qw (/home/mborto/perl5/lib/perl5/);
+use Algorithm::Combinatorics qw(combinations);
+use Getopt::Std;
+
+my @expenses;
+my $expenseLength = 0;
+
+open (INPUT, "puzzleInput.txt") || die "Can't open puzzleInput";
+#open (INPUT, "puzzleSample.txt") || die "Can't open puzzleInput";
+
+while (my $line = <INPUT>) {
+    chomp($line);
+    push(@expenses, $line); 
+}
+
+compute_answer(\@expenses, 2);
+compute_answer(\@expenses, 3);
+
+sub compute_answer {
+    my ($list, $comboSize) = @_;    
+
+    print "ComboSize is: $comboSize\n";
+
+    my $iter = combinations($list, $comboSize);
+
+    while (my $c = $iter->next) {
+        my $sum    = 0; #initialize to zero for sums
+        my $answer = 1; #initialize to one for multiplication
+
+        foreach my $entry (@{$c}) {
+            $sum    += $entry;
+            $answer *= $entry;
+        }
+
+        if ($sum == 2020) {
+            print "@{$c}\n";
+            print "Sum is: $sum\n";
+            print "Answer is: $answer\n\n";
+            last;
+        }
+    }
+}
+
+exit (0);
+
